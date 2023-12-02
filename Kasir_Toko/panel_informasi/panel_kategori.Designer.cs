58fcc2864940de6582c6e669cf3ff4e1fc70be60
dwi cahyo <dwicahyo.1512@gmail.com>
2023-12-02 11:41:52 +0700

    add kategori

--- Kasir_Toko/panel_informasi/panel_kategori.Designer.cs
+++ Kasir_Toko/panel_informasi/panel_kategori.Designer.cs
@@ -28,39 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.Search_tabel = new System.Windows.Forms.TextBox();
+            this.search_label = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_kategori = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.kategori_keterangan = new System.Windows.Forms.RichTextBox();
+            this.kategori_nama = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button10 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.clear_button);
+            this.panel3.Controls.Add(this.Search_tabel);
+            this.panel3.Controls.Add(this.search_label);
             this.panel3.Controls.Add(this.dataGridView1);
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Controls.Add(this.richTextBox1);
-            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.kategori_keterangan);
+            this.panel3.Controls.Add(this.kategori_nama);
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.label1);
             this.panel3.Controls.Add(this.button10);
             this.panel3.Controls.Add(this.button9);
             this.panel3.Controls.Add(this.button8);
-            this.panel3.Location = new System.Drawing.Point(-1, -2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(839, 477);
+            this.panel3.Size = new System.Drawing.Size(838, 475);
             this.panel3.TabIndex = 8;
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(253, 124);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(85, 28);
+            this.clear_button.TabIndex = 15;
+            this.clear_button.Text = "Clear form";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // Search_tabel
+            // 
+            this.Search_tabel.Location = new System.Drawing.Point(433, 61);
+            this.Search_tabel.Name = "Search_tabel";
+            this.Search_tabel.Size = new System.Drawing.Size(258, 22);
+            this.Search_tabel.TabIndex = 14;
+            this.Search_tabel.TextChanged += new System.EventHandler(this.Search_tabel_TextChanged);
+            // 
+            // search_label
+            // 
+            this.search_label.AutoSize = true;
+            this.search_label.Location = new System.Drawing.Point(376, 61);
+            this.search_label.Name = "search_label";
+            this.search_label.Size = new System.Drawing.Size(50, 16);
+            this.search_label.TabIndex = 13;
+            this.search_label.Text = "Search";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.id_kategori,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(376, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(433, 327);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.DataPropertyName = "id_kategori";
+            this.Column1.HeaderText = "#";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 43;
+            // 
+            // id_kategori
+            // 
+            this.id_kategori.HeaderText = "Column4";
+            this.id_kategori.MinimumWidth = 6;
+            this.id_kategori.Name = "id_kategori";
+            this.id_kategori.ReadOnly = true;
+            this.id_kategori.Visible = false;
+            this.id_kategori.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "Kategori";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 86;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Kategori Keterangan";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
             // 
             // label3
             // 
@@ -73,22 +175,22 @@
             this.label3.Text = "Kategori";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // richTextBox1
+            // kategori_keterangan
             // 
-            this.richTextBox1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(114, 256);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(224, 47);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
+            this.kategori_keterangan.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kategori_keterangan.Location = new System.Drawing.Point(114, 256);
+            this.kategori_keterangan.Name = "kategori_keterangan";
+            this.kategori_keterangan.Size = new System.Drawing.Size(224, 47);
+            this.kategori_keterangan.TabIndex = 6;
+            this.kategori_keterangan.Text = "";
             // 
-            // textBox1
+            // kategori_nama
             // 
-            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(115, 193);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(223, 23);
-            this.textBox1.TabIndex = 5;
+            this.kategori_nama.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kategori_nama.Location = new System.Drawing.Point(115, 193);
+            this.kategori_nama.Name = "kategori_nama";
+            this.kategori_nama.Size = new System.Drawing.Size(223, 23);
+            this.kategori_nama.TabIndex = 5;
             // 
             // label2
             // 
@@ -119,6 +221,7 @@
             this.button10.TabIndex = 2;
             this.button10.Text = "update";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button9
             // 
@@ -129,6 +232,7 @@
             this.button9.TabIndex = 1;
             this.button9.Text = "create";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button8
             // 
@@ -139,61 +243,7 @@
             this.button8.TabIndex = 0;
             this.button8.Text = "delete";
             this.button8.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Yellow;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.Desktop;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(376, 89);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(434, 327);
-            this.dataGridView1.TabIndex = 10;
-            // 
-            // Column1
-            // 
-            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.Column1.HeaderText = "#";
-            this.Column1.MinimumWidth = 6;
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            this.Column1.Width = 43;
-            // 
-            // Column2
-            // 
-            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.Column2.HeaderText = "Kategori";
-            this.Column2.MinimumWidth = 6;
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            this.Column2.Width = 86;
-            // 
-            // Column3
-            // 
-            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column3.HeaderText = "Kategori Keterangan";
-            this.Column3.MinimumWidth = 6;
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // panel_kategori
             // 
@@ -204,6 +254,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "panel_kategori";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.panel_kategori_Load);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -215,16 +266,20 @@
 
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox kategori_keterangan;
+        private System.Windows.Forms.TextBox kategori_nama;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox Search_tabel;
+        private System.Windows.Forms.Label search_label;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_kategori;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button clear_button;
     }
 }
--- Kasir_Toko/panel_informasi/panel_kategori.cs
+++ Kasir_Toko/panel_informasi/panel_kategori.cs
@@ -1,18 +1,160 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Windows.Forms;
 
 namespace Kasir_Toko.panel_informasi
 {
     public partial class panel_kategori : Form
     {
+        public string Id_kategori;
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+        int i = 0;
+
+        dbconnection dbconn = new dbconnection();
+
         public panel_kategori()
         {
             InitializeComponent();
+            conn = new MySqlConnection(dbconn.dbconnect());
+        }
+        private void panel_kategori_Load(object sender, EventArgs e)
+        {
+            loadRecord();
+        }
+        public void loadRecord()
+        {
+            dataGridView1.Rows.Clear();
+            conn.Open();
+            cmd = new MySqlCommand("SELECT kategori_id, kategori_nama, kategori_ket FROM kategori", conn);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Add(dataGridView1.Rows.Count + 1, dr["kategori_id"].ToString(), dr["kategori_nama"].ToString(), dr["kategori_ket"].ToString());
+            }
+            dr.Close();
+            conn.Close();
+        }
+
+        public void clear()
+        {
+            kategori_nama.Clear();
+            kategori_keterangan.Clear();
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //create
+            if ((kategori_nama.Text == string.Empty) || (kategori_keterangan.Text == string.Empty))
+            {
+                MessageBox.Show("Warning : Isi form kategori", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+
+            }
+            else
+            {
+                conn.Open();
+                cmd = new MySqlCommand("INSERT INTO kategori (kategori_id, kategori_nama, kategori_ket) VALUES(NULL,@kategori_nama, @kategori_keterangan)", conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@kategori_nama", kategori_nama.Text);
+                cmd.Parameters.AddWithValue("@kategori_keterangan", kategori_keterangan.Text);
+
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Berhasil di save!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Tidak Berhasil di save!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                conn.Close();
+                loadRecord();
+                clear();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //update
+            conn.Open();
+            cmd = new MySqlCommand("UPDATE kategori SET `kategori_nama` =@kategori_nama, `kategori_ket` =@kategori_keterangan WHERE kategori_id =@id", conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@kategori_nama", kategori_nama.Text);
+            cmd.Parameters.AddWithValue("@kategori_keterangan", kategori_keterangan.Text);
+            cmd.Parameters.AddWithValue("@id", Id_kategori);
+
+            i = cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                MessageBox.Show("Berhasil di Update!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Tidak Berhasil di Update!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            conn.Close();
+            loadRecord();
+            clear();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //delete
+            conn.Open();
+            cmd = new MySqlCommand("Delete from kategori WHERE kategori_id=@id", conn);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@id", Id_kategori);
+
+
+            i = cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                MessageBox.Show("Berhasil di Hapus!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Tidak Berhasil di Hapus!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            conn.Close();
+            loadRecord();
+            clear();
+
+        }
+
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            Id_kategori = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            kategori_nama.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            kategori_keterangan.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void Search_tabel_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            conn.Open();
+            cmd = new MySqlCommand("SELECT kategori_id, kategori_nama, kategori_ket FROM kategori WHERE  kategori_id like '%" + Search_tabel.Text + "%' or kategori_nama like '%" + Search_tabel.Text + "%' or kategori_ket like '%" + Search_tabel.Text + "%' ", conn);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Add(dataGridView1.Rows.Count + 1, dr["kategori_id"].ToString(), dr["kategori_nama"].ToString(), dr["kategori_ket"].ToString());
+            }
+            dr.Close();
+            conn.Close();
+        }
+
+
     }
-}
+    }
